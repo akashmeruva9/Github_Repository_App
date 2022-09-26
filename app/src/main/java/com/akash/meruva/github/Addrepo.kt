c72c285b450f7a120c3217ca536eb0efc47e351c
Akash Meruva <aksh.meruva2003@gmail.com>
2022-09-26 22:08:16 +0530

    some changes

--- app/src/main/java/com/akash/meruva/github/Addrepo.kt
+++ app/src/main/java/com/akash/meruva/github/Addrepo.kt
@@ -11,8 +11,6 @@ import java.lang.Exception
 
 
 class Addrepo : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addrepo)
